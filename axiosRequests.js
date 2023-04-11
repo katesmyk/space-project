033750de10dfa11aa81f990c66a1d4f471690c4d
Kate Smyk <smyk.k@brights.io>
2023-04-11 20:59:55 +0300

    Updates

--- axiosRequests.js
+++ axiosRequests.js
@@ -1,88 +1,85 @@
+const planetListEl = document.querySelector("#planetList");
+const imgFolder = "static/img/planets/";
 
-// getPlanetsData();
+const options = {
+  method: "GET",
+  url: "https://cloud.seatable.io/dtable-server/api/v1/dtables/9de50710-8482-48e8-9e1c-dc5573536a97/rows/",
+  params: {table_name: "Table1"},
+  headers: {
+    accept: "application/json",
+    authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODEzNzg1MzcsImR0YWJsZV91dWlkIjoiOWRlNTA3MTAtODQ4Mi00OGU4LTllMWMtZGM1NTczNTM2YTk3IiwidXNlcm5hbWUiOiIiLCJwZXJtaXNzaW9uIjoicnciLCJhcHBfbmFtZSI6Im15YXBwIn0.nP1Hxs692UHF6w0-D47a91lGuOADCUucvMnmvYO5y4g",
+  }
+};
 
-// function setPlanetsData() {
-//   const planetImgEl = document.querySelector("#planetImg");
-//   const planetNameEl = document.querySelector("#planetName");
-//   const sunDistanceEl = document.querySelector("#sunDistance span");
-//   const planetRadiusEl = document.querySelector("#radius span");
-//   const planetPopulationEl = document.querySelector("#population span");
+const planetTemplate = (planet) => `
+  <div class="products__item-inner">
+    <div class="products__item-img">
+      <picture>
+        <source srcset="${imgFolder}${planet.planet_Name}.webp" type="image/webp">
+        <img src="${imgFolder}${planet.planet_Name}.png" alt="${planet.planet_Name}">
+      </picture>
+    </div>
+    <div class="products__item-info">
+      <h2>${planet.planet_Name}</h2>
+      <p>Sun distance: <span>${planet.sun_Distance}</span> M km</p>
+      <p>Radius: <span>${planet.planet_Radius}</span> km</p>
+      <p>Population: <span>${planet.planet_Population}</span> billions</p>
+    </div>
+    <div class="products__item-buttons">
+      <button class="products__item button--blue">Edit</button>
+      <button class="products__item button--red">Delete</button>
+    </div>
+  </div>
+`;
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     const options = {
-//       method: "GET",
-//       url: "https://cloud.seatable.io/dtable-server/api/v1/dtables/9de50710-8482-48e8-9e1c-dc5573536a97/rows/",
-//       params: {table_name: "Table1"},
-//       headers: {
-//         accept: "application/json",
-//         authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODEzNzg1MzcsImR0YWJsZV91dWlkIjoiOWRlNTA3MTAtODQ4Mi00OGU4LTllMWMtZGM1NTczNTM2YTk3IiwidXNlcm5hbWUiOiIiLCJwZXJtaXNzaW9uIjoicnciLCJhcHBfbmFtZSI6Im15YXBwIn0.nP1Hxs692UHF6w0-D47a91lGuOADCUucvMnmvYO5y4g",
-//       }
-//     };
-
-//     axios
-//       .request(options)
-//       .then(function (response) {
-//         const planets = response.data.rows;
-//         console.log(planets);
-//         const planet = planets[0];
-//         planetImgEl.src = planet.img;
-//         planetNameEl.innerHTML = planet.planet_Name;
-//         sunDistanceEl.innerHTML = planet.sun_Distance;
-//         planetRadiusEl.innerHTML = planet.planet_Radius;
-//         planetPopulationEl.innerHTML = planet.planet_Population;
-//       })
-//       .catch(function (error) {
-//         console.error(error);
-//       });
-//   });
-// }
-
-// updated function
-function setPlanetsData() {
-  const planetListEl = document.querySelector("#planetList");
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const options = {
-      method: "GET",
-      url: "https://cloud.seatable.io/dtable-server/api/v1/dtables/9de50710-8482-48e8-9e1c-dc5573536a97/rows/",
-      params: {table_name: "Table1"},
-      headers: {
-        accept: "application/json",
-        authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODEzNzg1MzcsImR0YWJsZV91dWlkIjoiOWRlNTA3MTAtODQ4Mi00OGU4LTllMWMtZGM1NTczNTM2YTk3IiwidXNlcm5hbWUiOiIiLCJwZXJtaXNzaW9uIjoicnciLCJhcHBfbmFtZSI6Im15YXBwIn0.nP1Hxs692UHF6w0-D47a91lGuOADCUucvMnmvYO5y4g",
-      }
-    };
-
+function fetchData() {
     axios
       .request(options)
       .then(function (response) {
         const planets = response.data.rows;
-        const imgFolder = "static/img/planets/";
-        // <img src="${planet.img}" alt="${planet.planet_Name}"></img>
+        
         planetListEl.innerHTML = "";
         for (const planet of planets) {
           const planetEl = document.createElement("div");
           planetEl.classList.add("products__item");
-          planetEl.innerHTML = `
-            <div class="products__item-inner">
-              <div class="products__item-img">
-                <picture>
-                  <source srcset="${imgFolder}${planet.planet_Name}.webp" type="image/webp">
-                  <img src="${imgFolder}${planet.planet_Name}.png" alt="${planet.planet_Name}">
-                </picture>
-              </div>
-              <div class="products__item-info">
-                <h2>${planet.planet_Name}</h2>
-                <p>Sun distance: <span>${planet.sun_Distance}</span> M km</p>
-                <p>Radius: <span>${planet.planet_Radius}</span> km</p>
-                <p>Population: <span>${planet.planet_Population}</span></p>
-              </div>
-              <div class="products__item-buttons">
-                <button class="products__item button--blue">Edit</button>
-                <button class="products__item button--red">Delete</button>
-              </div>
-            </div>
-          `;
+          planetEl.innerHTML = planetTemplate(planet);
           planetListEl.appendChild(planetEl);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+}
+
+document.addEventListener("DOMContentLoaded", fetchData);
+
+function searchProduct() {
+  const searchInput = document.querySelector(".search__input > input[type=text]");
+
+  searchInput.addEventListener("input", function () {
+    axios
+      .request(options)
+      .then(function (response) {
+        const planets = response.data.rows;
+        const searchValue = searchInput.value.toLowerCase();
+        // Clear the planet list
+        planetListEl.innerHTML = "";
+        
+        // Filter planets by name
+        const filteredPlanets = planets.filter(planet => {
+          return planet.planet_Name.toLowerCase().includes(searchValue);
+        });
+
+        if (filteredPlanets.length === 0) {
+          planetListEl.innerHTML = "<p>No results found.</p>";
+        } else {
+          // Display search results
+          filteredPlanets.forEach(planet => {
+            const planetEl = document.createElement("div");
+            planetEl.classList.add("products__item");
+            planetEl.innerHTML = planetTemplate(planet);
+            planetListEl.appendChild(planetEl);
+          });
         }
       })
       .catch(function (error) {
@@ -91,4 +88,71 @@ function setPlanetsData() {
   });
 }
 
-setPlanetsData()
+searchProduct();
+
+
+function cancelSearch() {
+  const searchInput = document.querySelector(".search__input > input[type=text]");
+  const cancelButtonEl = document.querySelector(".search__button.button--red");
+
+  cancelButtonEl.addEventListener("click", function () {
+    planetListEl.innerHTML = "";
+    searchInput.value = "";
+    fetchData();
+  });
+}
+
+cancelSearch();
+
+
+function sortByRadius() {
+  const sortToggler = document.querySelector('.toggler .toggle__input');
+  sortToggler.addEventListener('change', function () {
+    console.log(sortToggler.checked);
+    if (sortToggler.checked) {
+      axios
+        .request(options)
+        .then(function (response) {
+          let planets = response.data.rows;
+          sortedPlanets = planets.sort(function (a, b) {
+            let radiusA = a.planet_Radius;
+            let radiusB = b.planet_Radius;
+            return radiusA - radiusB;
+            
+          });
+          planetListEl.innerHTML = "";
+          for (const planet of sortedPlanets) {
+            const planetEl = document.createElement("div");
+            planetEl.classList.add("products__item");
+            planetEl.innerHTML = planetTemplate(planet);
+            planetListEl.appendChild(planetEl);
+          }
+        });
+    } else {
+      fetchData();
+    }
+  });
+}
+
+sortByRadius();
+
+function countPlanetPopulation() {
+  const populationBtn = document.querySelector("#countBtn");
+  const populationEl = document.querySelector("#countTotalVal > span");
+  
+  populationBtn.addEventListener("click", function () {
+    axios
+      .request(options)
+      .then(function (response) {
+        const planets = response.data.rows;
+        let totalPopulation = 0;
+        planets.forEach(planet => {
+          totalPopulation += parseInt(planet.planet_Population);
+        });
+        populationEl.textContent = totalPopulation;
+      });
+  });
+}
+
+countPlanetPopulation();
+
--- script.js
+++ script.js
@@ -14,90 +14,90 @@
 // Дані по товарах повинні зберігатися в БД (наприклад, при додаванні нового товару йде відповідний запит на сервер і відбувається запис даних в БД).
 
 // Search by name
-let searchInput = document.querySelector(".search__input > input[type=text]");
-let searchButton = document.querySelector(".search__button.button--blue");
-let productsContent = document.querySelectorAll(".products__content .products__item");
+// let searchInput = document.querySelector(".search__input > input[type=text]");
+// let searchButton = document.querySelector(".search__button.button--blue");
+// let productsContent = document.querySelectorAll(".products__content .products__item");
 
-function searchByName() {
-  let searchValue = searchInput.value.toLowerCase();
+// function searchByName() {
+//   let searchValue = searchInput.value.toLowerCase();
 
-  productsContent.forEach((product) => {
-    let productNameElement = product.querySelector(".products__item-info h2");
+//   productsContent.forEach((product) => {
+//     let productNameElement = product.querySelector(".products__item-info h2");
     
-    if (productNameElement) {
-      let productName = productNameElement.innerText.toLowerCase();
+//     if (productNameElement) {
+//       let productName = productNameElement.innerText.toLowerCase();
   
-      if (productName.includes(searchValue)) {
-        product.classList.remove("hidden");
-      } else {
-        product.classList.add("hidden");
-      }
-    }
-  });
-}
+//       if (productName.includes(searchValue)) {
+//         product.classList.remove("hidden");
+//       } else {
+//         product.classList.add("hidden");
+//       }
+//     }
+//   });
+// }
 
-let cancelButton = document.querySelector(".search__button.button--red");
-let noResults = "I can't find anything :("
+// let cancelButton = document.querySelector(".search__button.button--red");
+// let noResults = "I can't find anything :("
 
-cancelButton.addEventListener("click", function () {
-  searchInput.value = "";
-  let productCards = document.querySelectorAll(".products__item");
-  productCards.forEach(function (productCard) {
-   	productCard.classList.remove("hidden");
-  });
+// cancelButton.addEventListener("click", function () {
+//   searchInput.value = "";
+//   let productCards = document.querySelectorAll(".products__item");
+//   productCards.forEach(function (productCard) {
+//    	productCard.classList.remove("hidden");
+//   });
 
-//   TODO: no results
-});
+// //   TODO: no results
+// });
 
-searchButton.addEventListener("click", searchByName);
+// searchButton.addEventListener("click", searchByName);
 
 // Sort by radius
-const sortToggler = document.querySelector('.toggler input[type="checkbox"]');
+// const sortToggler = document.querySelector('.toggler input[type="checkbox"]');
 
-function sortByRadius() {
-  let planets = document.querySelectorAll(".products__item");
-  let planetList = Array.from(planets); // Convert NodeList to Array
-  let sortedPlanets = planetList.sort((a, b) => {
-    // Get radius values for each planet
-    let radiusAElement = a.querySelector(".products__item-info > p:nth-child(3) > span");
-    let radiusBElement = b.querySelector(".products__item-info > p:nth-child(3) > span");
-    let radiusA = parseInt(radiusAElement?.innerText || 0);
-    let radiusB = parseInt(radiusBElement?.innerText || 0);
-    return radiusB - radiusA; // Sort in descending order
-  });
-  return sortedPlanets;
-}
+// function sortByRadius() {
+//   let planets = document.querySelectorAll(".products__item");
+//   let planetList = Array.from(planets); // Convert NodeList to Array
+//   let sortedPlanets = planetList.sort((a, b) => {
+//     // Get radius values for each planet
+//     let radiusAElement = a.querySelector(".products__item-info > p:nth-child(3) > span");
+//     let radiusBElement = b.querySelector(".products__item-info > p:nth-child(3) > span");
+//     let radiusA = parseInt(radiusAElement?.innerText || 0);
+//     let radiusB = parseInt(radiusBElement?.innerText || 0);
+//     return radiusB - radiusA; // Sort in descending order
+//   });
+//   return sortedPlanets;
+// }
 
-sortToggler.addEventListener("change", () => {
-  const planetContainer = document.querySelector(".products__content");
-  const planets = document.querySelectorAll(".products__item");
+// sortToggler.addEventListener("change", () => {
+//   const planetContainer = document.querySelector(".products__content");
+//   const planets = document.querySelectorAll(".products__item");
 
-  if (sortToggler.checked) {
-    // Sort planets by radius and add to container
-    let sortedPlanets = sortByRadius();
-    sortedPlanets.forEach((planet) => {
-      // Move the buttons inside the planet element
-      let buttons = planet.querySelector(".products__item-buttons");
-      planet.appendChild(buttons);
-      planetContainer.appendChild(planet);
+//   if (sortToggler.checked) {
+//     // Sort planets by radius and add to container
+//     let sortedPlanets = sortByRadius();
+//     sortedPlanets.forEach((planet) => {
+//       // Move the buttons inside the planet element
+//       let buttons = planet.querySelector(".products__item-buttons");
+//       planet.appendChild(buttons);
+//       planetContainer.appendChild(planet);
 
-		let inner = planet.querySelector(".products__item-inner");
-		inner.classList.add("active");
-    });
-  } else {
-    // Reset to original order
-      planets.forEach((planet) => {
-      // Move the buttons back 
-        let buttons = planet.querySelector(".products__item-buttons");
-        planet.querySelector(".products__item-inner").appendChild(buttons);
-        planetContainer.appendChild(planet);
+// 		let inner = planet.querySelector(".products__item-inner");
+// 		inner.classList.add("active");
+//     });
+//   } else {
+//     // Reset to original order
+//       planets.forEach((planet) => {
+//       // Move the buttons back 
+//         let buttons = planet.querySelector(".products__item-buttons");
+//         planet.querySelector(".products__item-inner").appendChild(buttons);
+//         planetContainer.appendChild(planet);
 
-		  let innerElements = planet.querySelector(".products__item-inner");
-      innerElements.forEach((inner) => {
-        inner.classList.remove("active");
-      });
-    });
-  }
-  // TODO: soring isnt reseting to original order
-});
+// 		  let innerElements = planet.querySelector(".products__item-inner");
+//       innerElements.forEach((inner) => {
+//         inner.classList.remove("active");
+//       });
+//     });
+//   }
+//   // TODO: soring isnt reseting to original order
+// });
 
